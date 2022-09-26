902968048fedbae7b55203ec69fbb00480133503
Eiden <eidnegabriel07@gmail.com>
2022-09-26 16:03:46 -0400

    fix task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,13 @@
 $(document).ready(function() {
   const checked = [];
-  let amenities = 0
-  /*const ameniti = document.getElementsByName('data-name');*/
   $('.amenities :checkbox').change(function(){
-      if ($('.amenities :checkbox').prop('checked')){
-        console.log($('.amenities :checkbox')[0].nextSibling.data)
-        /*checked.push(amenity.getAttribute('data-name'))*/
-        $('.amenities h4').append(`${checked}`)
+      if ($(this).prop('checked')){
+        checked.push(($(this).data().name))
+        $('.amenities h4').text(`${checked}`)
       }
-      if ($('.amenities :checkbox').prop('unchecked')){
-        console.log('unchecked')
+      else {
+        checked.splice(checked.indexOf($(this).data().name), 1);
+        $('.amenities h4').text(`${checked}`)
       }
   });
 });
